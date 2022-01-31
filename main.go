a22b1ce3a3da4cd407c36e610f2c17fb821a06f0
Homyak <semerenkok@rambler.ru>
2022-01-31 11:46:25 +0300

    "!" added

--- main.go
+++ main.go
@@ -12,10 +12,10 @@ func main() {
 
 	m := make(map[string]*student.Student)
 	studentStorage := storage.Storage{}
-	fmt.Println("Введите имя, возраст и курс студента")
+	fmt.Println("Введите имя, возраст и курс студента!")
 	for {
 		scanner := bufio.NewScanner(os.Stdin)
-		if !scanner.Scan(){
+		if !scanner.Scan() {
 			break
 		}
 		newStudent := scanner.Text()
